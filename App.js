a4d714e4ad4e9538648879a274facc8dac76fea0
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2020-05-16 16:01:38 +0530

    Added bottom nav bar

--- App.js
+++ App.js
@@ -1,19 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function App() {
+
+function Donate() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Donate</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function PMCares() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>PM Cares</Text>
+    </View>
+  );
+}
+
+function Contact() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>LandLine and other contact details</Text>
+    </View>
+  );
+}
+
+
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Donate"
+      activeColor="white"
+      labelStyle={{ fontSize: 12 }}
+      barStyle={{backgroundColor:'black'}}
+    >
+      <Tab.Screen
+        
+        name="Donate"
+        component={Donate}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="PMCares"
+        component={PMCares}
+        options={{
+          tabBarLabel: 'PM Cares',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="gift" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Contact"
+        component={Contact}
+        options={{
+          tabBarLabel: 'Contact Us',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
+
+export default function App() {
+  return (
+    
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+    
+  );
+}
