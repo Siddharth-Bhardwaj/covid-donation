e8b840f6a98dcde18110ed0b07251fffcd99e55b
akshatje <43758221+akshatje@users.noreply.github.com>
2020-05-17 18:08:45 +0530

    splash icon change
    splash icon change

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './components/Login'
-import SignUp from './components/SignUp'
-import Reset from './components/Reset'
-import Home from './components/Home'
-
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Reset from "./components/Reset";
+import Home from "./components/Home";
 
 function Donate() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Donate</Text>
     </View>
   );
@@ -20,7 +19,7 @@ function Donate() {
 
 function PMCares() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>PM Cares</Text>
     </View>
   );
@@ -28,12 +27,11 @@ function PMCares() {
 
 function Contact() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>LandLine and other contact details</Text>
     </View>
   );
 }
-
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -43,14 +41,13 @@ function MyTabs() {
       initialRouteName="Donate"
       activeColor="white"
       labelStyle={{ fontSize: 12 }}
-      barStyle={{backgroundColor:'#3267fa'}}
+      barStyle={{ backgroundColor: "#3267fa" }}
     >
       <Tab.Screen
-        
         name="Donate"
         component={Home}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: "Home",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="home" color={color} size={26} />
           ),
@@ -60,7 +57,7 @@ function MyTabs() {
         name="PMCares"
         component={PMCares}
         options={{
-          tabBarLabel: 'PM Cares',
+          tabBarLabel: "PM Cares",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="gift" color={color} size={24} />
           ),
@@ -70,7 +67,7 @@ function MyTabs() {
         name="Contact"
         component={Contact}
         options={{
-          tabBarLabel: 'Contact Us',
+          tabBarLabel: "Contact Us",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="account" color={color} size={26} />
           ),
@@ -80,19 +77,29 @@ function MyTabs() {
   );
 }
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} options={{ title: '' ,headerTransparent:'true'}}></Stack.Screen>
-        <Stack.Screen name="Sign Up" component={SignUp} options={{ title: '' ,headerTransparent:'true'}}></Stack.Screen>
-        <Stack.Screen name="Reset" component={Reset} options={{ title: '' ,headerTransparent:'true'}}></Stack.Screen>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: "", headerTransparent: "true" }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Sign Up"
+          component={SignUp}
+          options={{ title: "", headerTransparent: "true" }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="Reset"
+          component={Reset}
+          options={{ title: "", headerTransparent: "true" }}
+        ></Stack.Screen>
         <Stack.Screen name="Dashboard" component={MyTabs}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
-    
   );
 }
--- components/Login.js
+++ components/Login.js
@@ -1,121 +1,193 @@
-import React from 'react';
-import { NativeModules, StatusBarIOS ,StyleSheet, Text, View ,Button, Alert , Image, Keyboard, Dimensions, TextInput , TouchableOpacity  ,TouchableWithoutFeedback} from 'react-native';
-import { CommonActions } from '@react-navigation/native';
-import axios from 'axios';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-const { StatusBarManager } = NativeModules
+import React from "react";
+import {
+  NativeModules,
+  StatusBarIOS,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  Image,
+  Keyboard,
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { CommonActions } from "@react-navigation/native";
+import axios from "axios";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { apisAreAvailable } from "expo";
+const { StatusBarManager } = NativeModules;
 
+class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    statusBarHeight: 0,
+  };
 
+  logIn = async () => {
+    var username = this.state.username;
+    var password = this.state.password;
+    // var url = "http://172.20.10.3:80/login/?username="+username+"&password="+password;
+    // var resp = await axios.get(url);
+    // console.log(resp);
+    console.log(this.state.username);
+    console.log(this.state.password);
+    this.props.navigation.dispatch(
+      CommonActions.reset({
+        index: 0,
+        routes: [{ name: "Dashboard" }],
+      })
+    );
+  };
 
-class Login extends React.Component
-{
-
-state = {
-username : '',
-password : '',
-statusBarHeight : 0
-};
-
-
-logIn = async () => {
-
-  var username = this.state.username;
-  var password = this.state.password;
-  // var url = "http://172.20.10.3:80/login/?username="+username+"&password="+password;
-  // var resp = await axios.get(url);
-  // console.log(resp);
-  console.log(this.state.username);
-  console.log(this.state.password);
-  this.props.navigation.dispatch(
-    CommonActions.reset({
-      index: 0,
-      routes: [
-        { name: 'Dashboard'
-        },
-      ],
-    })
-  );
-  }
-
-  componentDidMount () {
-    if (Platform.OS === 'ios') {
-      StatusBarManager.getHeight(response =>
-          this.setState({statusBarHeight: response.height})
-      )
-      this.listener = StatusBarIOS.addListener('statusBarFrameWillChange',
+  componentDidMount() {
+    if (Platform.OS === "ios") {
+      StatusBarManager.getHeight((response) =>
+        this.setState({ statusBarHeight: response.height })
+      );
+      this.listener = StatusBarIOS.addListener(
+        "statusBarFrameWillChange",
         (statusBarData) =>
-          this.setState({statusBarHeight: statusBarData.frame.height})
-      )
-    }
-  }
-  
-  componentWillUnmount () {
-    if (Platform.OS === 'ios' && this.listener) {
-      this.listener.remove()
+          this.setState({ statusBarHeight: statusBarData.frame.height })
+      );
     }
   }
 
-navSignUp = () => {
-  this.props.navigation.navigate('Sign Up');
-}
-
-navReset = () => {
-this.props.navigation.navigate('Reset');
-}
-
-  render()
-  {
-    if(this.state.statusBarHeight==40)
-    { 
-      var height =  (Math.round(Dimensions.get('window').height))-(this.state.statusBarHeight)/2;
-      var width = (Math.round(Dimensions.get('window').width))-(this.state.statusBarHeight)/2;
+  componentWillUnmount() {
+    if (Platform.OS === "ios" && this.listener) {
+      this.listener.remove();
     }
-    else
-    {
-      var height =  (Math.round(Dimensions.get('window').height))
-      var width = (Math.round(Dimensions.get('window').width))
+  }
+
+  navSignUp = () => {
+    this.props.navigation.navigate("Sign Up");
+  };
+
+  navReset = () => {
+    this.props.navigation.navigate("Reset");
+  };
+
+  render() {
+    if (this.state.statusBarHeight == 40) {
+      var height =
+        Math.round(Dimensions.get("window").height) -
+        this.state.statusBarHeight / 2;
+      var width =
+        Math.round(Dimensions.get("window").width) -
+        this.state.statusBarHeight / 2;
+    } else {
+      var height = Math.round(Dimensions.get("window").height);
+      var width = Math.round(Dimensions.get("window").width);
     }
-    return(
+    return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View style={{flex:1,backgroundColor:'white'}}> 
-      <KeyboardAwareScrollView>
-        <View>
-          <Text style={{textAlign:'center',marginTop:height*0.17,fontSize:35}}>Marketplace</Text>
+        <View style={{ flex: 1, backgroundColor: "white" }}>
+          <KeyboardAwareScrollView>
+            <View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  marginTop: height * 0.17,
+                  fontSize: 35,
+                }}
+              >
+                MARKETPLACE
+              </Text>
+            </View>
+            <View style={{ alignItems: "center", marginTop: height * 0.05 }}>
+              <Image
+                style={{ width: 120, height: 120 }}
+                source={require("./../images/people.png")}
+              />
+            </View>
+            <View style={{ alignItems: "center", marginTop: height * 0.1 }}>
+              <TextInput
+                value={this.state.username}
+                onChangeText={(text) => this.setState({ username: text })}
+                style={{
+                  height: 40,
+                  backgroundColor: "white",
+                  borderBottomWidth: 2,
+                  borderColor: "gray",
+                  width: width * 0.85,
+                  fontSize: 27,
+                  paddingBottom: 10,
+                }}
+                placeholder="Username"
+                placeholderTextColor="#949494"
+              ></TextInput>
+            </View>
+            <View style={{ alignItems: "center", marginTop: height * 0.05 }}>
+              <TextInput
+                value={this.state.password}
+                onChangeText={(text) => this.setState({ password: text })}
+                secureTextEntry={true}
+                style={{
+                  height: 42,
+                  backgroundColor: "white",
+                  borderBottomWidth: 2,
+                  borderColor: "gray",
+                  width: width * 0.85,
+                  fontSize: 27,
+                  paddingBottom: 10,
+                }}
+                placeholder="Password"
+                placeholderTextColor="#949494"
+              ></TextInput>
+            </View>
+            <View style={{ alignItems: "center", marginTop: height * 0.05 }}>
+              <TouchableOpacity
+                onPress={this.logIn}
+                style={{
+                  borderWidth: 1,
+                  width: width * 0.25,
+                  borderRadius: 5,
+                  height: height * 0.05,
+                  backgroundColor: "#4a78ff",
+                  borderColor: "#4a78ff",
+                }}
+              >
+                <View style={{ flex: 1, justifyContent: "center" }}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      textAlign: "center",
+                      color: "white",
+                    }}
+                  >
+                    Login
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={{ alignItems: "center", marginTop: height * 0.025 }}>
+              <TouchableOpacity onPress={this.navReset}>
+                <Text style={{ fontSize: 18, color: "gray" }}>
+                  Need Help Logging In?
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignSelf: "center",
+                position: "absolute",
+                top: height * 0.94,
+              }}
+            >
+              <Text style={{ fontSize: 18 }}>Don't have an account? </Text>
+              <TouchableOpacity onPress={this.navSignUp}>
+                <Text style={{ fontSize: 18, color: "#4a78ff" }}>Sign up.</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAwareScrollView>
         </View>
-        <View style={{alignItems:'center',marginTop:height*0.05}}>
-        <Image
-              style={{width : 75, height: 75}}
-              source={require('./../images/marketplace.jpg')}
-            />
-        </View>
-      <View style={{alignItems:'center',marginTop:height*0.10}}>
-        <TextInput value={this.state.username} onChangeText={(text) => this.setState({username: text})}  style={{height:40,backgroundColor:'white',borderBottomWidth:2,borderColor:'gray',width:width*0.85,fontSize:27,paddingBottom:10}} placeholder="Username" placeholderTextColor="#949494"></TextInput>
-      </View>
-      <View style={{alignItems:'center',marginTop:height*0.05}}>
-        <TextInput value={this.state.password} onChangeText={(text) => this.setState({password: text})} secureTextEntry={true} style={{height:42,backgroundColor:'white',borderBottomWidth:2,borderColor:'gray',width:width*0.85,fontSize:27,paddingBottom:10}} placeholder="Password" placeholderTextColor="#949494"></TextInput>
-      </View>
-      <View style={{alignItems:'center',marginTop:height*0.05}}>
-        <TouchableOpacity onPress={this.logIn} style={{borderWidth:1,width:width*0.25,borderRadius:5,height:height*0.05,backgroundColor:'#4a78ff',borderColor:'#4a78ff'}}>
-          <View style={{flex:1,justifyContent:'center'}}>
-            <Text style={{fontSize:20,textAlign:'center',color:'white'}}>Login</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View style={{alignItems:'center',marginTop:height*0.025}}>
-        <TouchableOpacity onPress={this.navReset}><Text style={{fontSize:18,color:'gray'}}>Forgot your password?</Text></TouchableOpacity>
-      </View>
-      <View style={{flexDirection:'row',alignSelf:'center',position:'absolute',top:height*0.94}}>
-
-        <Text style={{fontSize:18}}>Don't have an account? </Text>
-        <TouchableOpacity onPress={this.navSignUp}>
-          <Text style={{fontSize:18,color:"#4a78ff"}}>Sign up.</Text>
-        </TouchableOpacity>
-      </View>
-      </KeyboardAwareScrollView>
-      </View>
       </TouchableWithoutFeedback>
     );
-    }
-};
+  }
+}
 
 export default Login;
